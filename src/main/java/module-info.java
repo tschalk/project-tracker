2991b3c9fac4870ddf6565e2a3e6462244002194
tschalk <119447814+tschalk@users.noreply.github.com>
2023-05-26 19:48:31 +0200

    update project data

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.github.tschalk.projecttracker {
     requires javafx.fxml;
     requires java.sql;
     requires javafx.base;
+    requires org.jetbrains.annotations;
     opens com.github.tschalk.project_tracker.model to javafx.base;
     opens com.github.tschalk.project_tracker to javafx.graphics;
 
